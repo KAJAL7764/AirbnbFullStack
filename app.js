9efd928de61c5212c74d24c8a03baed9fc631dc2
kajal kumari <128963957+KAJAL7764@users.noreply.github.com>
2025-06-20 11:06:02 +0530

    fixes small issues

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ const sessionOptions = {
     },
 }
 
-app.get('/', (req, res) => {
-    res.send('This is root routes');
-})
+// app.get('/', (req, res) => {
+//     res.send('This is root routes');
+// })
 
 app.use(session(sessionOptions));
 app.use(flash());
